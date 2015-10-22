65314e3c1f60ef158942d9e49b62f1ae8943a532
jeremystodghill <jeremystodghill@gmail.com>
2015-10-22 08:11:39 -0400

    rtsadr

---
+++ JobTest/UserPreferences.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Master.Master" CodeBehind="UserPreferences.aspx.cs" Inherits="JobTest.UserPreferences" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="Main" runat="server">
+
+  <form id="form1" runat="server">
+
+  <div id="recentorders" class="toprightheader inlinelink" style="margin-top:60px;width:760px;">
+            <div style="border-radius: 8px; ">
+                <h1 style="margin-left: 10px; margin-bottom:10px ; font-family: 'Trebuchet MS' !important;">User Preferences&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <asp:Label ID="lblResult" runat="server" Font-Bold="True" ForeColor="Blue" Visible="False"></asp:Label>
+                </h1>
+            </div>
+            
+            <div class="noresultfound">
+              
+             
+              <strong>Email Address:&nbsp;
+              <asp:TextBox ID="txtEmail" runat="server" Width="264px"></asp:TextBox>
+              <asp:Button ID="btnSave2" runat="server" Font-Bold="True" OnClick="btnSave_Click" Text="Save" />
+              &nbsp;</strong></div>
+
+</div>
+</form>
+</asp:Content>
+
---
+++ JobTest/UserPreferences.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JobTest
+{
+  public partial class UserPreferences : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      if (Page.IsPostBack == false)
+      {
+        //hard-coding UserID just for test, assuming it came over from login
+        Session["UserId"] = 1;
+
+        int intUserID = Convert.ToInt16(Session["UserId"]);
+        //Load Users preferences
+        LoadUser(intUserID);
+
+      }
+    }
+
+
+
+    public void LoadUser(int UserID)
+    {
+      NationalBuilderSupplyData.NBSDataDataContext mydb = new NationalBuilderSupplyData.NBSDataDataContext();
+      NationalBuilderSupplyData.NBSUser user = mydb.NBSUsers.Single(u => u.UserID == UserID);
+      txtEmail.Text = user.email;
+    }
+
+
+
+     public bool UpdateUser(string email)
+    {
+      if (txtEmail.Text != "")
+      {
+        bool blSuccess = true;
+        try
+        {
+          NationalBuilderSupplyData.NBSDataDataContext mydb = new NationalBuilderSupplyData.NBSDataDataContext();
+
+          var users = from a in mydb.NBSUsers
+                  where a.UserID == Convert.ToInt16(Session["UserId"].ToString())
+                  select a;
+
+          foreach (NationalBuilderSupplyData.NBSUser us in users)
+          {
+            us.email = email;
+          }
+
+          // Submit the changes to the database.
+
+            mydb.SubmitChanges();
+          lblResult.Visible = true;
+          lblResult.Text = "user email updated!";
+        
+        }
+        catch (Exception ex)
+        {
+          //log error somewhere
+          blSuccess = false;
+        }
+        return blSuccess;
+      }
+      else
+      {
+
+        return false;
+      }
+    }
+
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+      UpdateUser(txtEmail.Text);
+    }
+
+  
+
+
+
+  }
+}
